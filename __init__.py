1adc7587659e625326b9d16340421292b67a83b6
v1aduct <akifakb@gmail.com>
2025-08-11 22:01:21 +0300

    Deleted models.py and __init__.py due to reasons stated in previous commits, added a requirements.txt file

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-db = SQLAlchemy()
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/testdb'
-db.init_app(app)
