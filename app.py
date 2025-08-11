16c8c4043841e2501436143a96093ea66b298096
v1aduct <akifakb@gmail.com>
2025-08-11 21:56:12 +0300

    Moved the contents of models.py and __init.py__ into app.py in order to resolve some importing issue that will be adressed later Used the crawl method in crawl.py to finish up AddWebPage route, currently adds appends the url with the amount of suburls to the database Added the found URLs to the URL added response In the __name__ == __main__ function added database.create_all() in order to form the tables automatically if not already formed

--- app.py
+++ app.py
@@ -1,30 +1,60 @@
-from . import app, db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask import request, redirect, url_for,make_response
-from .models import WebPage
 import subprocess
-from . import crawl
+from crawl import crawl
 
 
-@app.route('/AddWebPage', methods=['POST'])
+
+flaskapp = Flask(__name__)
+
+database = SQLAlchemy()
+flaskapp.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@db:5432/testdb'
+database.init_app(flaskapp)
+
+
+class WebPage(database.Model):
+    __tablename__ = 'webpages'
+    
+    id = database.Column(database.Integer, primary_key=True)
+    url = database.Column(database.String(255), nullable=False, unique=True)
+    subURLCount =  database.Column(database.Integer)
+    created_at = database.Column(database.DateTime, server_default=database.func.now())
+    
+    def __repr__(self):
+        return f'<WebPage {self.url}>'
+
+    
+
+
+
+@flaskapp.route('/AddWebPage', methods=['POST'])
 def add_webpage():
     data = request.json
     url = data.get('url')
-    subURLCount = crawl(url)[1]
+    crawlReturn = crawl(url)
+    subURLCount = crawlReturn[1]
     if url and subURLCount:
         webpage = WebPage(
             url = url,
             subURLCount = subURLCount
         )
-        db.session.add(webpage)
-        db.session.commit()
+        database.session.add(webpage)
+        database.session.commit()
         return make_response(
-            {"message":"URL added"},
+            {"message":"URL added",
+             "URLS found":crawlReturn[0]},
             200
         )
     return make_response(
         {"message":"Data was not entered correctly"},
         500
     )
+
+if __name__ == "__main__":
+    with flaskapp.app_context():
+        database.create_all()
+    flaskapp.run(host="0.0.0.0", port=5000)
 
     
 
